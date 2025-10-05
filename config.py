9838fe2acf778e6ada9e15fc9bca9caf1c795fbb
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-10-05 19:32:35 +0100

    changement  db

--- config.py
+++ config.py
@@ -2,12 +2,11 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="b5wukeymkwlavs8t0nmf-mysql.services.clever-cloud.com",
-        user="u3zvttr70dqoe1mn",
-        password="Fn1GDAzBgRgvaU5bFyMG",
-        database="b5wukeymkwlavs8t0nmf",
-        port="3306"
-        
+        host="bjvb74nnosa1zevqpwuf-mysql.services.clever-cloud.com",
+        user="uil0vd8bkjjojqek",
+        password="Iru7niERRTD7MWBuL0R4",
+        database="bjvb74nnosa1zevqpwuf",
+        port="3306" 
     )
 
 
