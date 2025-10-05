0e53cf02d044a0663b82dcc4f3125440f63fbaeb
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-10-05 19:58:56 +0100

    Mise à jour mode connexion avec  db

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="bjvb74nnosa1zevqpwuf-mysql.services.clever-cloud.com",
-        user="uil0vd8bkjjojqek",
-        password="Iru7niERRTD7MWBuL0R4",
-        database="bjvb74nnosa1zevqpwuf",
-        port="3306" 
-    )
+DB_CONFIG = {
+    'host': 'bjvb74nnosa1zevqpwuf-mysql.services.clever-cloud.com',
+    'user': 'uil0vd8bkjjojqek',
+    'password': 'Iru7niERRTD7MWBuL0R4',
+    'database': 'bjvb74nnosa1zevqpwuf',
+    'port': '3306'
+}
 
 
 
