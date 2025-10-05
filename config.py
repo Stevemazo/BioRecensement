d854418e254e48e1d2323fe602d2f986f78f12e5
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-10-05 18:57:07 +0100

    modification mot de passe db

--- config.py
+++ config.py
@@ -4,9 +4,16 @@ def get_connection():
     return mysql.connector.connect(
         host="b5wukeymkwlavs8t0nmf-mysql.services.clever-cloud.com",
         user="u3zvttr70dqoe1mn",
-        password="u3zvttr70dqoe1mn",
+        password="Fn1GDAzBgRgvaU5bFyMG",
         database="b5wukeymkwlavs8t0nmf",
         port="3306"
         
     )
+
+
+
+
+
+
+
 
